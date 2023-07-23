14b7e97fc9d94c0ee63288856c05dc1f0b450533
MichaelThomas0721 <michaelthomas0721@icloud.com>
2023-07-23 17:24:48 +0000

    Started working on implementing a friends system

--- src/components/reusable/FrostedTextInput.svelte
+++ src/components/reusable/FrostedTextInput.svelte
@@ -1,16 +1,29 @@
 <script>
     export let placeholder;
+    export let fullRound = false;
+    export let icon = "";
+    export let iconClass = "";
     export let bind = "";
+    export let label = true;
 </script>
 
 <span class="flex flex-col">
-    <label for={placeholder + "Input"} class="text-xl pl-1">{placeholder}</label
+    {#if label}<label for={placeholder + "Input"} class="text-xl pl-1"
+            >{placeholder}</label
+        >{/if}
+    <span
+        class={`w-full h-12 ${
+            fullRound ? "rounded-full" : "rounded-md"
+        } bg-white bg-opacity-10 shadow-xl text-lg outline-none p-3 px-5 flex flex-row items-center`}
     >
-    <input
-        id={placeholder + "Input"}
-        name={placeholder}
-        bind:value={bind}
-        class="w-full h-12 rounded-md bg-white bg-opacity-10 shadow-xl text-lg outline-none p-3"
-        {placeholder}
-    />
+        <input
+            id={placeholder + "Input"}
+            name={placeholder}
+            bind:value={bind}
+            class={`bg-transparent flex-grow outline-none`}
+            {placeholder}
+        />
+        {#if icon}<iconify-icon {icon} class={`overflow-hidden ${iconClass}`} />
+        {/if}
+    </span>
 </span>
---
+++ src/routes/friends/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import FrostedTextInput from "$components/reusable/FrostedTextInput.svelte";
+    import ApiFetcher from "$services/ApiFetcher";
+    let search = "";
+    let results = [];
+
+    async function SearchFriends() {
+        results = await ApiFetcher("/api/fetch-mongo", {
+            params: { email: search },
+            collection: "users",
+        });
+    }
+</script>
+
+<div>
+    <h1>Friends</h1>
+    <form on:submit={SearchFriends}>
+        <FrostedTextInput
+            placeholder="Find Friends"
+            label={false}
+            fullRound={true}
+            bind:bind={search}
+            icon={"material-symbols:search"}
+            iconClass="-scale-x-100 text-gray-400"
+        />
+    </form>
+</div>
