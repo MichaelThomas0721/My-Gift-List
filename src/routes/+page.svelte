434f671fd8566586cd0ecc6eb0e705e208484ea8
MichaelThomas0721 <michaelthomas0721@icloud.com>
2023-07-27 13:23:24 +0000

    Added signup button on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,22 @@
 
 <script>
   import Head from "$components/reusable/Head.svelte";
-import navaction from "$root/lib/navaction";
+  import navaction from "$root/lib/navaction";
   $navaction = null;
 </script>
 
 <Head title="Home" />
-<div class="">
-  <div />
+<div class="flex flex-col items-center">
   <h1
-    class="text-[11vw] font-bold text-white text-center bg-gradient-to-b from-darkGreen to-darkGreen"
+    class="text-[10vw] font-bold text-white text-center bg-gradient-to-b from-darkGreen to-darkGreen"
   >
     There is no homepage 0.0
   </h1>
+  <a
+    href="/signup"
+    class="bg-lightPurple rounded-md px-3 py-2 text-xl text-center mx-auto"
+    >Signup</a
+  >
 </div>
 
 <style lang="postcss">
