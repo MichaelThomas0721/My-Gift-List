bd0c66e904db3dee2993d4617308574044a71c30
MichaelThomas0721 <michaelthomas0721@icloud.com>
2023-08-15 19:27:48 +0000

    feat(dev): started declaring some types

--- src/routes/signup/+page.server.ts
+++ src/routes/signup/+page.server.ts
@@ -5,32 +5,33 @@ import FetchMongo from '$services/FetchMongo.js';
 import CreateIndexMongo from '$services/CreateIndexMongo.js';
 import { SECRET_SETUP } from '$env/static/private';
 import Hash from '$services/Hash.js';
+import type { PageLoad } from '../$types.js';
 
-export const load = async ({ cookies }) => {
+export const load: PageLoad = async ({ cookies }: any) => {
     if (cookies.get('user')) {
         throw redirect(302, '/profile')
     }
 }
 
 export const actions = {
-    signup: async ({ cookies, request }) => {
+    signup: async ({ cookies, request }: any) => {
         const data = await request.formData()
-        const email = data.get('EmailInput');
-        const username = data.get('UsernameInput');
-        const password = data.get('PasswordInput');
+        const email: String = data.get('EmailInput');
+        const username: String = data.get('UsernameInput');
+        const password: String = data.get('PasswordInput');
         if (!email || !username || !password) return fail(400, { credentials: true })
-        let hashedPassword = await Hash(String(password));
-        let params = { "email": email, "username": username, "password": hashedPassword }
+        let hashedPassword: String = await Hash(String(password));
+        let params: Object = { "email": email, "username": username, "password": hashedPassword }
         if (SECRET_SETUP) {
             await CreateIndexMongo({ "username": 1 }, { "unique": true }, "users");
             await CreateIndexMongo({ "email": 1 }, { "unique": true }, "users");
         }
         if ((await FetchMongo({ username }, "users"))[0]) return fail(403, { errorMsg: 'Username Taken' })
         if ((await FetchMongo({ email }, "users"))[0]) return fail(403, { errorMsg: 'Email already in use' })
-        let rData = await AddMongo(params, "users");
+        let rData: any = await AddMongo(params, "users");
         if (!rData) return fail(403, { errorMsg: 'Error adding to data, please try again later' });
 
-        let user = await FetchMongo({ _id: rData.insertedId }, "users")
+        let user: any = await FetchMongo({ _id: rData.insertedId }, "users")
         user = user[0];
         let _id = await AddObjectId(user._id)
         await AddMongo({ _id, name: `${user.username}'s Wishlist`, userId: user._id }, "lists");
