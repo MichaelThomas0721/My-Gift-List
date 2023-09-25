c2d86579e542f946e2b6e13385663ba6ddcaf464
MichaelThomas0721 <michaelthomas0721@icloud.com>
2023-09-25 14:41:41 +0000

    test: logs

--- src/services/ApiFetcher.ts
+++ src/services/ApiFetcher.ts
@@ -8,6 +8,7 @@ import { PUBLIC_ABS_URL } from "$env/static/public";
 export default async function ApiFetcher(path, params) {
     // Try to fetch
     try {
+        console.log(params);
         // Conduct the fetch, have to use JSON.stringify to be able to res.json()
         let res = await fetch(PUBLIC_ABS_URL + path, {
             method: 'POST',
@@ -18,6 +19,7 @@ export default async function ApiFetcher(path, params) {
             mode: 'no-cors',
             body: JSON.stringify(params)
         });
+        console.log(res);
         return res.json()
     } catch (e) {
         console.log(e)
