7d5ddf9310d77dfaf02c05cc7e8fc89bf30f98c9
MichaelThomas0721 <michaelthomas0721@icloud.com>
2023-09-25 15:55:15 +0000

    test: log

--- src/services/AddMongo.ts
+++ src/services/AddMongo.ts
@@ -5,21 +5,25 @@ import FetchMongo from "./FetchMongo";
 
 export default async function AddMongo(params, collection, user: any) {
     // Connect to mongodb
+    console.log("WATCH OU T");
     const client = await clientPromise;
     const db = client.db();
+    console.log("connected db");
     if (user != null) {
         if (typeof user == 'string') {
             user = JSON.parse(user);
         }
         const upUser = await FetchMongo({ username: user.username }, "users", true);
+        console.log(upUser);
         if (!upUser) return false;
         if (upUser.password != user.password) return false;
     } else if (collection != "users") {
         return false;
     }
-
+    console.log("YUH");
     // Add the data to the database or catch the error
     try {
+        console.log("ADD THIS SHIT");
         return await db.collection(collection).insertOne(params)
     } catch (e) {
         console.log(e)
