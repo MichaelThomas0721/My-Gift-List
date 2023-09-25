e6eabbec80b8ae519bfdab1c49e02e975321d3c6
MichaelThomas0721 <michaelthomas0721@icloud.com>
2023-09-25 13:51:24 +0000

    feat: made the random confetti better

---
+++ src/components/index/Confetti.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let confetti;
+</script>
+
+<div
+    class="border-8 border-t-[15px] w-0 h-0 absolute"
+    style={`transform: rotate(${confetti.rotation}deg); top: ${confetti.top}%; left: ${confetti.left}%; border-color: ${confetti.color} transparent transparent transparent`}
+/>
---
+++ src/components/index/RandomConfetti.svelte
@@ -0,0 +1,32 @@
+<script>
+    import Confetti from "./Confetti.svelte";
+    const colors = ["#FF718D", "#FDFF6A", "#A864FD", "#7FCDF9", "#78FF44"]
+    let confettis = GenerateConfetti(15);
+
+    function GenerateConfetti(length) {
+        let c = [];
+        for (let i = 0; i < length; i++) {
+            c.push(OneConfetti());
+        }
+        return c;
+    }
+
+    function OneConfetti() {
+        let c = {};
+        c.rotation = RandomInteger(360);
+        c.left = RandomInteger(100);
+        c.top = RandomInteger(80);
+        c.color = colors[RandomInteger(colors.length-1)];
+        return c;
+    }
+
+    function RandomInteger(range) {
+        return Math.floor(Math.random() * (range - 0 + 1));
+    }
+</script>
+
+<div class="w-full aspect-[10/3] relative">
+    {#each confettis as confetti}
+        <Confetti {confetti} />
+    {/each}
+</div>
