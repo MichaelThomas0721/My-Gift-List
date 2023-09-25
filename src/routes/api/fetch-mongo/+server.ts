4291f80f5c601657ee8145eca0e0e2cc9509b42c
MichaelThomas0721 <michaelthomas0721@icloud.com>
2023-09-25 15:06:42 +0000

    test: log

--- src/routes/api/fetch-mongo/+server.ts
+++ src/routes/api/fetch-mongo/+server.ts
@@ -3,6 +3,7 @@ import FetchMongo from '$services/FetchMongo.js';
 
 export const POST = async ({ params, request }) => {
     const data = await request.json();
+    console.log(data);
     data.params = AddObjectId(data.params);
     let rData = await FetchMongo(data?.params, data?.collection);
     return new Response(JSON.stringify(rData), { status: 200 });
